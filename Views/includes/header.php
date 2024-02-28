b507854e7305bf727b32afd0f0e7ba866e8a12a5
Aurelien229 <blampainaurelien229@gmail.com>
2024-02-28 09:08:39 +0100

    morning

---
+++ Views/includes/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./Assets/src/output.css" rel="stylesheet">
+    <title>COGIP</title>
+</head>
+<body>
+    <nav class="navbar">
+        <h1>COGIP</h1>
+        <div class="nav-links">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Invoices</a></li>
+                <li><a href="#">Compagnies</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </div>
+    </nav>
+    <header>
+       
+    </header>
