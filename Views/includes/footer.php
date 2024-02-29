2aab4d94f9be8e260c0a015057955ce38cd08256
Aurelien229 <blampainaurelien229@gmail.com>
2024-02-29 10:20:44 +0100

    updateLink

--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -2,22 +2,22 @@
     <div class="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <div class="mb-4 md:mb-0">
             <p class="text-lg font-bold">COGIP</p>
-            <p>Square des Martyrs, 6000 Charleroi</p>
-            <p>(123) 456-7890</p>
-            <p>(123) 456-7890</p>
+            <p class="flex flex-row"><img class="mr-1" src="Assets/img/home.png" alt="Home">Square des Martyrs, 6000 Charleroi</p>
+            <p class="flex flex-row"><img class="mr-1" src="Assets/img/phone.png" alt="Phone">(123) 456-7890</p>
+            <p class="flex flex-row"><img class="mr-1" src="Assets/img/printer.png" alt="Fax">(123) 456-7890</p>
         </div>
         
         <div class="mb-4 md:mb-0">
             <p class="text-lg font-bold">Social Media</p>
             <div class="flex gap-4">
-                <a href="https://www.facebook.com/" target="_blank"><img src="Assets/img/facebook.png" alt="Facebook" class="h-6 w-6" /></a>
-                <a href="https://www.twitter.com/" target="_blank"><img src="Assets/img/twitter.png" alt="Twitter" class="h-6 w-6" /></a>
-                <a href="https://www.linkedin.com/" target="_blank"><img src="Assets/img/linkedin.png" alt="Linkedin" class="h-6 w-6" /></a>
-                <a href="https://www.linkedin.com/" target="_blank"><img src="Assets/img/youtube.png" alt="Youtube" class="h-6 w-6" /></a>
-                <a href="https://www.linkedin.com/" target="_blank"><img src="Assets/img/instagram.png" alt="Instagram" class="h-6 w-6" /></a>
-                <a href="https://www.linkedin.com/" target="_blank"><img src="Assets/img/google.png" alt="Google+" class="h-6 w-6" /></a>
-                <a href="https://www.linkedin.com/" target="_blank"><img src="Assets/img/pinterest.png" alt="Pinterest" class="h-6 w-6" /></a>
-                <a href="https://www.linkedin.com/" target="_blank"><img src="Assets/img/gmail.png" alt="Gmail" class="h-6 w-6" /></a>
+                <a class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100" href="https://www.facebook.com/" target="_blank"><img src="Assets/img/facebook.png" alt="Facebook" class="h-6 w-6" /></a>
+                <a class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100" href="https://www.twitter.com/" target="_blank"><img src="Assets/img/twitter.png" alt="Twitter" class="h-6 w-6" /></a>
+                <a class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100" href="https://www.linkedin.com/" target="_blank"><img src="Assets/img/linkedin.png" alt="Linkedin" class="h-6 w-6" /></a>
+                <a class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100" href="https://www.youtube.com/" target="_blank"><img src="Assets/img/youtube.png" alt="Youtube" class="h-6 w-6" /></a>
+                <a class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100" href="https://www.instagram.com/" target="_blank"><img src="Assets/img/instagram.png" alt="Instagram" class="h-6 w-6" /></a>
+                <a class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100" href="https://www.google.com/" target="_blank"><img src="Assets/img/google.png" alt="Google+" class="h-6 w-6" /></a>
+                <a class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100" href="https://www.pinterest.com/" target="_blank"><img src="Assets/img/pinterest.png" alt="Pinterest" class="h-6 w-6" /></a>
+                <a class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100" href="https://www.gmail.com/" target="_blank"><img src="Assets/img/gmail.png" alt="Gmail" class="h-6 w-6" /></a>
             </div>
         </div>
 
